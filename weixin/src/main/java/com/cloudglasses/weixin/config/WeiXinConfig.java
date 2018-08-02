e51f57d92e993b4dac03681f90162dd4afcbd56f
wanglei <wanglei@cheche365.com>
2018-08-02 11:19:52 +0800

    增加微信SDK使用

---
+++ weixin/src/main/java/com/cloudglasses/weixin/config/WeiXinConfig.java
@@ -0,0 +1,82 @@
+package com.cloudglasses.weixin.config;
+
+import com.cloudglasses.weixin.handler.*;
+import me.chanjar.weixin.common.api.WxConsts.EventType;
+import me.chanjar.weixin.common.api.WxConsts.MenuButtonType;
+import me.chanjar.weixin.common.api.WxConsts.XmlMsgType;
+import me.chanjar.weixin.mp.api.WxMpConfigStorage;
+import me.chanjar.weixin.mp.api.WxMpInMemoryConfigStorage;
+import me.chanjar.weixin.mp.api.WxMpMessageRouter;
+import me.chanjar.weixin.mp.api.WxMpService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.cloudglasses.weixin")
+@ConditionalOnClass(WxMpService.class)
+@EnableConfigurationProperties(WechatMpProperties.class)
+public class WeiXinConfig {
+    @Autowired
+    private WechatMpProperties properties;
+    @Autowired
+    protected LogHandler logHandler;
+    @Autowired
+    protected NullHandler nullHandler;
+    @Autowired
+    private MsgHandler msgHandler;
+    @Autowired
+    private UnsubscribeHandler unsubscribeHandler;
+    @Autowired
+    private SubscribeHandler subscribeHandler;
+
+    @Bean
+    @ConditionalOnMissingBean
+    public WxMpConfigStorage configStorage() {
+        WxMpInMemoryConfigStorage configStorage = new WxMpInMemoryConfigStorage();
+        configStorage.setAppId(this.properties.getAppId());
+        configStorage.setSecret(this.properties.getSecret());
+        configStorage.setToken(this.properties.getToken());
+        configStorage.setAesKey(this.properties.getAesKey());
+        return configStorage;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public WxMpService wxMpService(WxMpConfigStorage configStorage) {
+        WxMpService wxMpService = new me.chanjar.weixin.mp.api.impl.WxMpServiceImpl();
+        wxMpService.setWxMpConfigStorage(configStorage);
+        return wxMpService;
+    }
+
+    @Bean
+    public WxMpMessageRouter router(WxMpService wxMpService) {
+        final WxMpMessageRouter newRouter = new WxMpMessageRouter(wxMpService);
+
+        // 记录所有事件的日志 （异步执行）
+        newRouter.rule().handler(this.logHandler).next();
+
+        // 点击菜单连接事件
+        newRouter.rule().async(false).msgType(XmlMsgType.EVENT)
+                .event(MenuButtonType.VIEW).handler(this.nullHandler).end();
+
+        // 关注事件
+        newRouter.rule().async(false).msgType(XmlMsgType.EVENT)
+                .event(EventType.SUBSCRIBE).handler(this.subscribeHandler)
+                .end();
+
+        // 取消关注事件
+        newRouter.rule().async(false).msgType(XmlMsgType.EVENT)
+                .event(EventType.UNSUBSCRIBE)
+                .handler(this.unsubscribeHandler).end();
+
+        // 默认
+        newRouter.rule().async(false).handler(this.msgHandler).end();
+
+        return newRouter;
+    }
+}

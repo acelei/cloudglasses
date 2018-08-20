4d22382af250e77f97f3b2abfee5c2323ffd7e82
ace_lei@163.com <wlkou0120>
2018-08-20 22:37:18 +0800

    验证码加入小写字母

--- entity/src/main/java/com/cloudglasses/util/CodeUtile.java
+++ entity/src/main/java/com/cloudglasses/util/CodeUtile.java
@@ -18,8 +18,12 @@ public class CodeUtile {
     private static int xx = 15;
     private static int fontHeight = 18;
     private static int codeY = 16;
-    private static char[] codeSequence = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-            'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    private static char[] codeSequence = {
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+            'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+            's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+    };
 
     /**
      * 生成一个map集合
@@ -64,7 +68,7 @@ public class CodeUtile {
         // 随机产生codeCount数字的验证码。
         for (int i = 0; i < codeCount; i++) {
             // 得到随机产生的验证码数字。
-            String code = String.valueOf(codeSequence[random.nextInt(36)]);
+            String code = String.valueOf(codeSequence[random.nextInt(codeSequence.length)]);
             // 产生随机的颜色分量来构造颜色值，这样输出的每位数字的颜色值都将不同。
             red = random.nextInt(255);
             green = random.nextInt(255);
